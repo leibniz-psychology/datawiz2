aba75f3273428b2b70ba36378517c95ca0e5b976
Michael Cremer <coding@cremer.io>
2020-08-05 19:49:25 +0200

    Add security-bundle
    Starts to implement the oauth authentication logic.
    Needed for #14

---
+++ source/Security/Authentication/DevelopmentUserProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Security\Authentication;
+
+
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class DevelopmentUserProvider implements UserProviderInterface
+{
+    /**
+     * If you're not using these features, you do not need to implement
+     * this method.
+     *
+     * @return UserInterface
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     */
+    public function loadUserByUsername($username)
+    {
+        // TODO: Implement loadUserByUsername() method.
+    }
+
+    /**
+     * Refreshes the user after being reloaded from the session.
+     *
+     * When a user is logged in, at the beginning of each request, the
+     * User object is loaded from the session and then this method is
+     * called. Your job is to make sure the user's data is still fresh by,
+     * for example, re-querying for fresh User data.
+     *
+     * If your firewall is "stateless: true" (for a pure API), this
+     * method is not called.
+     *
+     * @return UserInterface
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        // TODO: Implement refreshUser() method.
+    }
+
+    /**
+     * Tells Symfony to use this provider for this User class.
+     */
+    public function supportsClass($class)
+    {
+        // TODO: Implement supportsClass() method.
+    }
+}
---
+++ source/Security/Authentication/OauthAuthenticator.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Security\Authentication;
+
+
+use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class OauthAuthenticator extends SocialAuthenticator
+{
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // TODO: Implement start() method.
+    }
+
+    public function supports(Request $request)
+    {
+        // TODO: Implement supports() method.
+    }
+
+    public function getCredentials(Request $request)
+    {
+        // TODO: Implement getCredentials() method.
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        // TODO: Implement getUser() method.
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // TODO: Implement onAuthenticationFailure() method.
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+}

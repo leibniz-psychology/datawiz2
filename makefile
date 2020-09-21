leibniz-psychology/datawiz2

commit c6ba2c6975d6ab024e1264bce707024afc4ee86b

Refactor deploy target to work with variables
Instead of two deploy target for a local server and a remote server, you can now choose.
The variables are user specific and the file must not exist to use the make targets.
The defaults are placeholders for now.

makefile
@@ -1,6 +1,10 @@
-# Variables
-ENV = dev # never use prod - NEVER
+include makevars
+# Variable defaults
+# Those can be changed with your own makevars file
+ENV ?= test # never use prod - NEVER
+INVENTORY ?= remote.ini # see infrastructure/inventory for valid options
 
+# Paths
 MARK_DIR = ./.markers
 VAR_DIR = ./var
 ANSIBLE_DIR = ./infrastructure
@@ -11,9 +15,9 @@ FIXTURES_DIR = $(DOMAIN_DIR)/Fixtures
 DEV_STATE_DIR = $(DOMAIN_DIR)/State/Development
 PROD_STATE_DIR = $(DOMAIN_DIR)/State/Production
 TEST_STATE_DIR = $(DOMAIN_DIR)/State/Test
-
 JS_DEPS = ./node_modules/
 PHP_DEPS = ./vendor/
+
 # Those files signal different states of the sqlite file
 SCHEMA_MARK = $(MARK_DIR)/schema
 FIXTURE_MARK = $(MARK_DIR)/fixture
@@ -49,13 +53,9 @@ migration: $(MIGRATION_MARK) ## Generate and apply a doctrine migration
 .PHONY: fixtures
 fixtures: $(FIXTURE_MARK) ## Apply doctrine fixtures
 
-.PHONY: deploy-local
-deploy-local: ## Run ansible for your local server
-	ansible-playbook $(ANSIBLE_DIR)/setup.yaml -i $(INVENTORY_DIR)/local.ini
-
-.PHONY: deploy-remote
-deploy-remote: ## Run ansible for the remote server
-	ansible-playbook $(ANSIBLE_DIR)/setup.yaml -i $(INVENTORY_DIR)/remote.ini
+.PHONY: deploy
+deploy: ## Run ansible for your local server
+	ansible-playbook $(ANSIBLE_DIR)/setup.yaml -i $(INVENTORY_DIR)/$(INVENTORY) -K
 
 .PHONY: clear
 clear: ## Delete all temporary files

.gitignore
@@ -269,8 +269,11 @@ source/Domain/State/Development
 # This directory tracks the state of actions which would
 # require one file to have multpile make targets
 .markers
-########
 
+# The user configuration of the makefile should not be shared
+makevars
+
+###
 
 ###> symfony/framework-bundle ###
 /.env.local

commit 09e7bbe27429b2e9309004d974424e27e44efea5

Fix broken help target
Commit c6ba2c6975d6ab024e1264bce707024afc4ee86b introduced this bug.
The issue arises because the content of $(MAKEFILE_LIST) changed by adding an include.
As soon as you include something, more than one file gets piped into the awk command.
This breaks the execution order.
The solution is to isolate the makefile filename from everything else.

makefile
@@ -14,6 +14,9 @@ LOCAL_IP ?= 0.0.0.0
 # IP of your remote server - this could be productions or testing
 REMOTE_IP ?= 0.0.0.0
 
+# name of this file
+SELF = $(firstword $(MAKEFILE_LIST))
+
 # Paths
 MARK_DIR = ./.markers
 VAR_DIR = ./var
@@ -45,7 +48,10 @@ MIGRATION_MARK = $(MARK_DIR)/migration
 # Thanks to Romain Gautier for his slides from symfony live 2018 providing this ->
 .DEFAULT_GOAL := help
 help: ## Show this help text
-	@grep -E '(^[a-zA-Z_-]+:.*?##.*$$)|(^##)' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}{printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}' | sed -e 's/\[32m##/[33m/'
+	@grep -E '(^[a-zA-Z_]+:.*?##.*$$)|(^##)' $(SELF) | awk 'BEGIN {FS = ":.*?## "}{printf "\033[32m%-30s\033[0m %s\n", $$1, $$2}' | sed -e 's/\[32m##/[33m/'
+
+debug:
+	echo $(MAKEFILE_LIST)
 
 ## General
 .PHONY: install
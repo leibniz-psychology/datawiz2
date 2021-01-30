cdddfb8100f518ca9a9e7c81059dbb71b330b189
Michael Cremer <coding@cremer.io>
2021-01-30 18:16:44 +0100

    Add simple functional tests for the StudyController
    These changes will check if each route if available.
    Additionally a dummy TestCase for security was created.
    Unfornately Symfony is not clear about the migration config and
    only without the test-migration config tests run fine.

---
+++ tests/Functional/SecurityFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityFeatureTest extends WebTestCase
+{
+    // TODO: Find out how to test security with Symfony 4.4
+    public function testSecurityDummy()
+    {
+        $this->assertEmpty('');
+    }
+}
---
+++ tests/Functional/StudyFeatureTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Tests\Unit\View\Controller;
+
+use App\View\Controller\StudyController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class StudyControllerTest
+ * This will check common functionality for the StudyController.
+ */
+class StudyFeatureTest extends WebTestCase
+{
+    private $client;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * Testing every Action within it's own method is verbose and should be avoided
+     * Better testing will be possible as soon as we can test a real workflow
+     * The following tests should only break if a url gets changed - which is not trivial to circumvent.
+     */
+    public function testIndexAction()
+    {
+        $this->client->request('GET', '/pages/studies');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testNewAction()
+    {
+        $this->client->request('GET', '/pages/studies/new');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testDocumentationAction()
+    {
+        $this->client->request('GET', '/pages/studies/documentation');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testAdminAction()
+    {
+        $this->client->request('GET', '/pages/studies/admin');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testDesignAction()
+    {
+        $this->client->request('GET', '/pages/studies/design');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testTheoryAction()
+    {
+        $this->client->request('GET', '/pages/studies/theory');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testSampleAction()
+    {
+        $this->client->request('GET', '/pages/studies/sample');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+}

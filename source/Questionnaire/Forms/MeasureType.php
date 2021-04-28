70e737c084813e00cf4c3cb9428f3c131dea761b
Florian Grässle <fg@leibniz-psychology.org>
2021-04-28 13:57:09 +0200

    Add styling to single metadata forms

--- source/Questionnaire/Forms/MeasureType.php
+++ source/Questionnaire/Forms/MeasureType.php
@@ -18,23 +18,38 @@ class MeasureType extends AbstractType
         $builder
             ->add(MetaDataDictionary::MEASURES,  CollectionType::class, [
                 'entry_type' => TextType::class,
-                'entry_options' => ['label'=>false],
+                'entry_options' => ['label' => false],
                 'allow_add' => true,
                 'prototype' => true,
                 'allow_delete' => true,
                 'label' => 'Tests and measures',
+                'label_attr' => ['class' => 'MetaData-Title'],
                 'help' => 'Provide definitions of all measures you collected on participants. Provide the proper name of standardized measures (tests).',
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'px-6 pt-1',
+                    'data-entry-add-label' => 'another measure +',
+                    'data-entry-add-class' => 'bg-transparent border-0 shadow-none hover:bg-transparent underline mx-auto block mt-1',
+                    'data-entry-remove-class' => ''
+                ]
             ])
             ->add(MetaDataDictionary::APPARATUS, CollectionType::class, [
                 'entry_type' => TextType::class,
-                'entry_options' => ['label'=>false],
+                'entry_options' => ['label' => false],
                 'allow_add' => true,
                 'prototype' => true,
                 'allow_delete' => true,
                 'label' => 'Apparatus (Instruments, Equipment)',
                 'help' => 'Which instruments (apparatus, equipment) did you use in your study?',
-            ])
-        ;
+                'label_attr' => ['class' => 'MetaData-Title'],
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'px-6 pt-1',
+                    'data-entry-add-label' => 'another apparatus +',
+                    'data-entry-add-class' => 'bg-transparent border-0 shadow-none hover:bg-transparent underline mx-auto block mt-1',
+                    'data-entry-remove-class' => ''
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- source/Questionnaire/Forms/MethodType.php
+++ source/Questionnaire/Forms/MethodType.php
@@ -17,29 +17,57 @@ class MethodType extends AbstractType
             ->add(MetaDataDictionary::SETTING, TextareaType::class, [
                 'label' => 'Setting',
                 'help' => '',
+                'label_attr' => ['class' => 'MetaData-Title'],
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'p-1',
+                ]
             ])
             ->add(MetaDataDictionary::RESEARCH_DESIGN, TextareaType::class, [
                 'label' => 'Research design',
                 'help' => '',
+                'label_attr' => ['class' => 'MetaData-Title'],
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'p-1',
+                ]
             ])
             ->add(MetaDataDictionary::MANIPULATIONS, TextareaType::class, [
                 'label' => 'Experimental manipulation(s), treatments, or interventions',
                 'help' => 'Provide details of your experimental manipulation(s) including a summary of the instructions. Clearly define your variables (treatments).',
+                'label_attr' => ['class' => 'MetaData-Title'],
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'p-1',
+                ]
             ])
             ->add(MetaDataDictionary::ASSIGNMENT, TextareaType::class, [
                 'label' => 'Assignment of participants to condition(s)/manipulation(s)',
                 'help' => 'How did you place participants in conditions? Select random or non-random',
+                'label_attr' => ['class' => 'MetaData-Title'],
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'p-1',
+                ]
             ])
             ->add(MetaDataDictionary::EXPERIMENTAL_DESIGN, TextareaType::class, [
                 'label' => 'Experimenttal?',
                 'help' => '',
+                'label_attr' => ['class' => 'MetaData-Title'],
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'p-1',
+                ]
             ])
             ->add(MetaDataDictionary::CONTROL_OPERATIONS, TextareaType::class, [
                 'label' => 'Control Operations?',
                 'help' => '',
-            ])
-
-        ;
+                'label_attr' => ['class' => 'MetaData-Title'],
+                'help_attr' => ['class' => 'px-6 pt-1'],
+                'attr' => [
+                    'class' => 'p-1',
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

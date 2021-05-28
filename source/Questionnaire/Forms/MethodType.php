b51ccfd684ecdced59295f1d08e7e32741141979
Michael Cremer <coding@cremer.io>
2021-05-28 20:03:05 +0200

    Fix wrong formtypes for Method-Metadata partly
    These changes introduce choice fields for sampling, research design and
    control operations.
    Related to issue #92

--- source/Questionnaire/Forms/MethodType.php
+++ source/Questionnaire/Forms/MethodType.php
@@ -5,6 +5,7 @@ namespace App\Questionnaire\Forms;
 use App\Domain\Definition\MetaDataDictionary;
 use App\Domain\Model\Study\MethodMetaDataGroup;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,13 @@ class MethodType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add(MetaDataDictionary::SETTING, TextareaType::class, [
+            ->add(MetaDataDictionary::SETTING, ChoiceType::class, [
+                'choices' => [
+                    'Artificial setting' => 'Artificial setting',
+                    'Real-life setting' => 'Real-life setting',
+                    'Natural setting' => 'Natural setting'
+                ],
+                'expanded' => true,
                 'label' => 'Setting',
                 'help' => '',
                 'label_attr' => ['class' => 'MetaData-Title'],
@@ -23,7 +30,14 @@ class MethodType extends AbstractType
                     'class' => 'p-1',
                 ]
             ])
-            ->add(MetaDataDictionary::RESEARCH_DESIGN, TextareaType::class, [
+            // TODO: Location Textfield was not mapped yet
+            ->add(MetaDataDictionary::RESEARCH_DESIGN, ChoiceType::class, [
+                'choices' => [
+                    'Independent measures / between-subjects design' => 'Independent measures / between-subjects design',
+                    'Repeated measures / within-subjects design' => 'Repeated measures / within-subjects design',
+                    'Matched pairs design' => 'Matched pairs design'
+                ],
+                'expanded' => true,
                 'label' => 'Research design',
                 'help' => '',
                 'label_attr' => ['class' => 'MetaData-Title'],
@@ -59,7 +73,17 @@ class MethodType extends AbstractType
                     'class' => 'p-1',
                 ]
             ])
-            ->add(MetaDataDictionary::CONTROL_OPERATIONS, TextareaType::class, [
+            ->add(MetaDataDictionary::CONTROL_OPERATIONS, ChoiceType::class, [
+                'choices' => [
+                    'Block randomization' => 'Block randomization',
+                    'Complete counterbalancing (all possible orders)' => 'Complete counterbalancing (all possible orders)',
+                    'Incomplete counterbalancing (partial counterbalancing)' => 'Incomplete counterbalancing (partial counterbalancing)',
+                    'Latin Square' => 'Latin Square',
+                    'Latin Square using a random starting order with rotation (rotate order)' => 'Latin Square using a random starting order with rotation (rotate order)',
+                    'Reverse counterbalancing (ABBA-counterbalancing)' => 'Reverse counterbalancing (ABBA-counterbalancing)',
+                    'Other' => 'Other'
+                ],
+                'expanded' => true,
                 'label' => 'Control Operations?',
                 'help' => '',
                 'label_attr' => ['class' => 'MetaData-Title'],
@@ -68,6 +92,7 @@ class MethodType extends AbstractType
                     'class' => 'p-1',
                 ]
             ]);
+        // TODO: Others free Textfield is not mapped yet and missing
     }
 
     public function configureOptions(OptionsResolver $resolver): void

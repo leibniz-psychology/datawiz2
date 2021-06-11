b0a1b47500092aa2b25ddd91d4344c42d3b103b2
Florian Grässle <fg@leibniz-psychology.org>
2021-06-11 11:55:51 +0200

    Apply textinput and widget styles

--- source/Questionnaire/Forms/MethodType.php
+++ source/Questionnaire/Forms/MethodType.php
@@ -17,19 +17,21 @@ class MethodType extends AbstractType
         $builder
             ->add(MetaDataDictionary::SETTING, ChoiceType::class, [
                 'required' => false,
+                'placeholder' => false,
                 'choices' => [
                     'Artificial setting' => 'Artificial setting',
                     'Real-life setting' => 'Real-life setting',
                     'Natural setting' => 'Natural setting'
                 ],
                 'expanded' => true,
-                'label' => 'Setting',
-                'help' => '',
-                'label_attr' => ['class' => 'MetaData-Title'],
-                'help_attr' => ['class' => 'px-6 pt-1'],
+                'label' => 'Choose your setting',
+                'label_attr' => ['class' => 'MetaData-Label'],
                 'attr' => [
                     'class' => 'p-1',
-                ]
+                ],
+                'choice_attr' => function () {
+                    return ['class' => 'RadioButton-Input'];
+                }
             ])
             // TODO: Location Textfield was not mapped yet
             ->add(MetaDataDictionary::RESEARCH_DESIGN, ChoiceType::class, [
@@ -50,23 +52,30 @@ class MethodType extends AbstractType
             ])
             ->add(MetaDataDictionary::MANIPULATIONS, TextareaType::class, [
                 'required' => false,
-                'label' => 'Experimental manipulation(s), treatments, or interventions',
-                'help' => 'Provide details of your experimental manipulation(s) including a summary of the instructions. Clearly define your variables (treatments).',
-                'label_attr' => ['class' => 'MetaData-Title'],
-                'help_attr' => ['class' => 'px-6 pt-1'],
+                'label' => 'Provide details of your experimental manipulation(s) including a summary of the instructions. Clearly define your variables (treatments).',
+                'label_attr' => ['class' => 'MetaData-Label'],
                 'attr' => [
-                    'class' => 'p-1',
+                    'class' => 'MetaData-TextInput',
                 ]
             ])
-            ->add(MetaDataDictionary::EXPERIMENTAL_DESIGN, TextareaType::class, [
+            ->add(MetaDataDictionary::EXPERIMENTAL_DESIGN, ChoiceType::class, [
+
                 'required' => false,
-                'label' => 'Experimenttal?',
-                'help' => '',
-                'label_attr' => ['class' => 'MetaData-Title'],
-                'help_attr' => ['class' => 'px-6 pt-1'],
+                'placeholder' => false,
+                'choices' => [
+                    'Independent measures / between-subjects design' => 'Independent measures / between-subjects design',
+                    'Repeated measures / within-subjects design' => 'Repeated measures / within-subjects design',
+                    'Matched pairs design' => 'Matched pairs design'
+                ],
+                'expanded' => true,
+                'label' => 'How were participants allocated to the different conditions?',
+                'label_attr' => ['class' => 'MetaData-Label'],
                 'attr' => [
                     'class' => 'p-1',
-                ]
+                ],
+                'choice_attr' => function () {
+                    return ['class' => 'RadioButton-Input'];
+                }
             ])
             ->add(MetaDataDictionary::CONTROL_OPERATIONS, ChoiceType::class, [
                 'required' => false,
@@ -80,13 +89,14 @@ class MethodType extends AbstractType
                     'Other' => 'Other'
                 ],
                 'expanded' => true,
-                'label' => 'Control Operations?',
-                'help' => '',
-                'label_attr' => ['class' => 'MetaData-Title'],
-                'help_attr' => ['class' => 'px-6 pt-1'],
+                'label' => 'Describes the actions taken to prevent sampling and order effects',
+                'label_attr' => ['class' => 'MetaData-Label'],
                 'attr' => [
                     'class' => 'p-1',
-                ]
+                ],
+                'choice_attr' => function () {
+                    return ['class' => 'RadioButton-Input'];
+                }
             ]);
         // TODO: Others free Textfield is not mapped yet and missing
     }

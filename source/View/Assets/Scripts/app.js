35d75e533a51cececde7f6af6d73273840d2a999
Florian Grässle <fg@leibniz-psychology.org>
2021-06-15 16:19:04 +0200

    Sort and organize imports

--- source/View/Assets/Scripts/app.js
+++ source/View/Assets/Scripts/app.js
@@ -6,16 +6,15 @@
  */
 
 // any CSS you import will output into a single Style file (app.Style in this case)
-import "../Styles/tailwind-base.scss";
-import "../Styles/app-base.scss";
-import "../Styles/tailwind-components.scss";
-import "../Styles/app-components.scss";
-import "../Styles/tailwind-utilities.scss";
-import "../Styles/app-utilities.scss";
-import "dropzone/dist/dropzone.css"
-
 import a2lix_lib from "@a2lix/symfony-collection/src/a2lix_sf_collection";
-import dropzone from 'dropzone'
+import "dropzone/dist/dropzone.css";
+import "../Styles/app-base.scss";
+import "../Styles/app-components.scss";
+import "../Styles/app-utilities.scss";
+import "../Styles/tailwind-base.scss";
+import "../Styles/tailwind-components.scss";
+import "../Styles/tailwind-utilities.scss";
+import "./alpine";
 
 a2lix_lib.sfCollection.init();
 

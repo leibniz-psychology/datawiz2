2f360814a69bc1c65c232301a6fb44736ed925a2
Florian Grässle <fg@leibniz-psychology.org>
2021-06-15 15:52:08 +0200

    Use markdown/raw html in radio button labels

---
+++ source/View/Templates/Twig/ErusevMarkdown.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\View\Templates\Twig;
+
+use ParsedownExtra;
+use Twig\Extra\Markdown\MarkdownInterface;
+
+class ErusevMarkdown implements MarkdownInterface
+{
+    private ParsedownExtra $converter;
+
+    public function __construct()
+    {
+        $this->converter = new ParsedownExtra();
+    }
+
+    public function convert(string $body): string
+    {
+        return $this->converter->text($body);
+    }
+}

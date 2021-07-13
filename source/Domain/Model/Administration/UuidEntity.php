8c2641515d8f14f4252ca262b3dd2dc05e792221
Michael Cremer <coding@cremer.io>
2021-07-13 15:41:42 +0200

    Update Symfony Framework to 5.3
    Fix Uuid changes.
    Hides PhpUnit deprecations caused by dependencies.

--- source/Domain/Model/Administration/UuidEntity.php
+++ source/Domain/Model/Administration/UuidEntity.php
@@ -3,8 +3,8 @@
 namespace App\Domain\Model\Administration;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\IdGenerator\UuidV4Generator;
-use Symfony\Component\Uid\UuidV4;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Component\Uid\Uuid;
 
 abstract class UuidEntity
 {
@@ -12,7 +12,7 @@ abstract class UuidEntity
      * @ORM\Id()
      * @ORM\Column(type="uuid")
      * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class=UuidV4Generator::class)
+     * @ORM\CustomIdGenerator(class=UuidGenerator::class)
      */
     protected $id;
 
@@ -21,13 +21,13 @@ abstract class UuidEntity
         return $this->id;
     }
 
-    public function uuidEquals(UuidV4 $uuid): bool
+    public function uuidEquals(Uuid $uuid): bool
     {
         return $uuid->equals($this->id);
     }
 
     public function uuidEqualsString(string $uuid): bool
     {
-        return UuidV4::fromString($uuid)->equals($this->id);
+        return Uuid::fromString($uuid)->equals($this->id);
     }
 }

d809ee161cfb144eac3fb37def579d8d71ea4c7a
Michael Cremer <coding@cremer.io>
2021-07-20 18:15:59 +0200

    Remove unneeded development users
    Reason: development login is not used by stakeholders anymore.

--- source/Domain/State/Fixtures/AppFixtures.php
+++ source/Domain/State/Fixtures/AppFixtures.php
@@ -13,14 +13,9 @@ class AppFixtures extends Fixture
     public function __construct()
     {
         $this->testUserNames = [
-            'mc@leibniz-psychology.org',
-            'fg@leibniz-psychology.org',
-            'user1@datawiz.org',
-            'user2@datawiz.org',
-            'user3@datawiz.org',
-            'user4@datawiz.org',
-            'user5@datawiz.org',
-            'user6@datawiz.org',
+            'user@leibniz-psychology.org',
+            'foo@leibniz-psychology.org',
+            'bar@leibniz-psychology.org',
         ];
     }
 

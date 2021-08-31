5f47419e53cc965831500f3e3f7a6c16bbad8d35
Florian Grässle <fg@leibniz-psychology.org>
2021-08-31 14:32:54 +0200

    Refactor css
    Due to performance reasons the tailwind imports were scattered among
    several files. With JIT this is no longer necessary.

--- source/View/Assets/Scripts/app.js
+++ source/View/Assets/Scripts/app.js
@@ -6,12 +6,9 @@
  */
 
 // any CSS you import will output into a single Style file (app.Style in this case)
-import "../Styles/tailwind-base.scss";
-import "../Styles/app-base.scss";
-import "../Styles/tailwind-components.scss";
-import "../Styles/app-components.scss";
-import "../Styles/tailwind-utilities.scss";
-import "../Styles/app-utilities.scss";
+import "tailwindcss/tailwind.css";
+import "../Styles/app.scss";
+
 import "dropzone/dist/dropzone.css";
 
 import a2lix_lib from "@a2lix/symfony-collection/src/a2lix_sf_collection";
@@ -23,20 +20,20 @@ import "./detectStickyElements";
 a2lix_lib.sfCollection.init();
 
 Dropzone.options.datawizDropzone = {
-	createImageThumbnails: false,
-	init: function () {
-		this.on("sending", function (file, xhr, formData) {
-			formData.append("originalFilename", file.name);
-			// require Templates/Components/_infoBridge.html.twig -> experiment.id as value
-			formData.append(
-				"studyId",
-				document.getElementById("infobridge").innerHTML.trim()
-			);
-		});
-		this.on("success", function (file, response) {
-			window.location.reload(true);
-		});
-	},
+  createImageThumbnails: false,
+  init: function () {
+    this.on("sending", function (file, xhr, formData) {
+      formData.append("originalFilename", file.name);
+      // require Templates/Components/_infoBridge.html.twig -> experiment.id as value
+      formData.append(
+        "studyId",
+        document.getElementById("infobridge").innerHTML.trim()
+      );
+    });
+    this.on("success", function (file, response) {
+      window.location.reload(true);
+    });
+  },
 };
 
 // console.log("Hello Webpack Encore! Edit me in Assets/Scripts/app.js"

e8e62f5bbe892400332734e6780afd3e5bbc6285
R Boelter <ronny.boelter@outlook.com>
2021-09-24 15:10:38 +0200

    Add missing migration

---
+++ source/Domain/State/Migrations/Development/Version20210924082926.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210924082926 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE experiment_basic_creators ADD credit_roles LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE experiment_basic_creators DROP credit_roles');
+    }
+}

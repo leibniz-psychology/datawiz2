2653f76f811cd781936202b8d1fb0543074efdd8
R Boelter <ronny.boelter@outlook.com>
2021-10-20 12:40:46 +0200

    Fix empty array error

--- source/View/Controller/CodebookController.php
+++ source/View/Controller/CodebookController.php
@@ -169,12 +169,14 @@ class CodebookController extends AbstractController
         if ($arr && is_iterable($arr) && key_exists('variables', $arr) && !empty($arr['variables']) && is_iterable($arr['variables'])) {
             foreach ($arr['variables'] as $variable) {
                 if (key_exists("var_db_id", $variable)) {
+                    $values = $variable["values"] ? array_values(array_filter($variable["values"])) : null;
+                    $missings = $variable["missings"] ? array_values(array_filter($variable["missings"])) : null;
                     $var = $this->em->getRepository(DatasetVariables::class)->find($variable["var_db_id"]);
                     $var->setName($variable["name"] ?? $var->getName());
                     $var->setLabel($variable["label"] ?? null);
                     $var->setItemText($variable["itemText"] ?? null);
-                    $var->setValues($variable["values"] ? array_values(array_filter($variable["values"])) : null);
-                    $var->setMissings($variable["missings"] ? array_values(array_filter($variable["missings"])) : null);
+                    $var->setValues(null != $values && 0 != sizeof($values) ? $values : null);
+                    $var->setMissings(null != $missings && 0 != sizeof($missings) ? $missings : null);
                     $var->setMeasure($variable["measure"] ?? null);
                     $this->em->persist($var);
                     $this->em->flush();

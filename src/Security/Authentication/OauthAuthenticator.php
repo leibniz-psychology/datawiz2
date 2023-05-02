390439b3dfb8dd6538d3b9767d74b0487c644512
Joel Beckmann <jb@leibniz-psychology.org>
2023-05-02 10:22:18 +0200

    chore: upgrade to sf6 security system

--- src/Security/Authentication/OauthAuthenticator.php
+++ src/Security/Authentication/OauthAuthenticator.php
@@ -6,24 +6,72 @@ use App\Crud\Crudable;
 use App\Domain\Definition\UserRoles;
 use App\Domain\Model\Administration\DataWizUser;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 use Symfony\Component\Uid\Uuid;
 
-class OauthAuthenticator extends SocialAuthenticator
+class OauthAuthenticator extends OAuth2Authenticator implements AuthenticationEntryPointInterface
 {
     use TargetPathTrait;
 
-    public function __construct(private readonly ClientRegistry $clientRegistry, private readonly Crudable $crud, private readonly UrlGeneratorInterface $urlGenerator)
+    public function __construct(
+        private readonly ClientRegistry $clientRegistry,
+        private readonly Crudable $crud,
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly EntityManagerInterface $entityManager,
+    )
     {
+    }
+
+    public function authenticate(Request $request): SelfValidatingPassport
+    {
+        $client = $this->clientRegistry->getClient('keycloak');
+        $accessToken = $this->fetchAccessToken($client);
+
+        return new SelfValidatingPassport(
+            new UserBadge($accessToken->getToken(), function() use ($accessToken, $client) {
+                $keycloakUser = $client->fetchUserFromToken($accessToken);
+
+                $user = null;
+                $kcArray = null;
+                if ($keycloakUser) {
+                    $user = $this->crud->readById(DataWizUser::class, $keycloakUser->getId());
+                    $kcArray = $keycloakUser->toArray();
+                }
+                if (is_iterable($kcArray)) {
+                    if ($user === null) {
+                        $user = new DataWizUser();
+                        $user->setId(new Uuid($keycloakUser->getId()));
+                        $user->setRoles([UserRoles::USER]);
+                        $user->setDateRegistered(new DateTime());
+                    }
+                    if (key_exists('email', $kcArray)) {
+                        $user->setEmail($kcArray['email']);
+                    }
+                    if (key_exists('given_name', $kcArray)) {
+                        $user->setFirstname($kcArray['given_name']);
+                    }
+                    if (key_exists('family_name', $kcArray)) {
+                        $user->setLastname($kcArray['family_name']);
+                    }
+                    $user->setLastLogin(new DateTime());
+                    $this->crud->update($user);
+                }
+
+                return $user;
+            })
+        );
     }
 
     public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
@@ -34,43 +82,6 @@ class OauthAuthenticator extends SocialAuthenticator
     public function supports(Request $request): bool
     {
         return $request->attributes->get('_route') === 'Security-check';
-    }
-
-    public function getCredentials(Request $request)
-    {
-        return $this->fetchAccessToken($this->clientRegistry->getClient('keycloak'));
-    }
-
-    public function getUser($credentials, UserProviderInterface $userProvider)
-    {
-        $keycloakUser = $this->clientRegistry->getClient('keycloak')->fetchUserFromToken($credentials);
-        $user = null;
-        $kcArray = null;
-        if ($keycloakUser) {
-            $user = $this->crud->readById(DataWizUser::class, $keycloakUser->getId());
-            $kcArray = $keycloakUser->toArray();
-        }
-        if (is_iterable($kcArray)) {
-            if ($user === null) {
-                $user = new DataWizUser();
-                $user->setId(new Uuid($keycloakUser->getId()));
-                $user->setRoles([UserRoles::USER]);
-                $user->setDateRegistered(new DateTime());
-            }
-            if (key_exists('email', $kcArray)) {
-                $user->setEmail($kcArray['email']);
-            }
-            if (key_exists('given_name', $kcArray)) {
-                $user->setFirstname($kcArray['given_name']);
-            }
-            if (key_exists('family_name', $kcArray)) {
-                $user->setLastname($kcArray['family_name']);
-            }
-            $user->setLastLogin(new DateTime());
-            $this->crud->update($user);
-        }
-
-        return $user;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response

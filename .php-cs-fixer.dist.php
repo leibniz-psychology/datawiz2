0202fb2ef9ce527c56eb34fd56527e5a03b585c3
Joel Beckmann <jb@leibniz-psychology.org>
2025-01-17 13:42:00 +0100

    ci: update php-cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,16 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->exclude(['var', 'vendor', 'node_modules'])
+    ->notPath('config/bundles.php')
+    ->notName('bundles.php')
     ->in(__DIR__)
-    ->exclude('var')
 ;
 
-$config = new PhpCsFixer\Config();
+$config = new Config();
 return $config->setRules([
     '@PhpCsFixer' => true,
     'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],

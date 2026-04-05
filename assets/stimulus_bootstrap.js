428a2f89f3b5a5c635b7163b0e1dfba8d548ff48
Joel Beckmann <jb@leibniz-psychology.org>
2026-04-05 09:17:06 +0200

    build: update symfony/stimulus-bundle recipe

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,6 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp();
 
 // register any custom, 3rd party controllers here
